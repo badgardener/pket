ecc8826eb50288dcb8aaccce0de0e8a56df8ab1d
Ali Ahmad <aliahmadjdr7.personal@gmail.com>
2026-09-19 21:35:37 +0530

    Removed code duplication.

--- callbacks.go
+++ callbacks.go
@@ -7,100 +7,61 @@ import (
 	"strings"
 )
 
-type NormalCallback struct{}
-type VerboseCallback struct{}
+type BaseCallback struct{}
 
-func (l NormalCallback) Log(msg string) {}
-func (l NormalCallback) Info(msg string) {
+func (l BaseCallback) Log(msg string) {}
+
+func (l BaseCallback) Info(msg string) {
 	fmt.Println(Info.Render("[ INFO  ]") + " " + msg)
 }
 
-func (l NormalCallback) Success(msg string) {
+func (l BaseCallback) Success(msg string) {
 	fmt.Println(Success.Render("[SUCCESS]") + " " + msg)
 }
 
-func (l NormalCallback) Warn(msg string) {
+func (l BaseCallback) Warn(msg string) {
 	fmt.Println(Warning.Render("[WARNING]") + " " + msg)
 }
 
-func (l NormalCallback) Error(msg string) {
+func (l BaseCallback) Error(msg string) {
 	fmt.Println(Error.Render("[ ERROR ]") + " " + msg)
 }
 
-func (l NormalCallback) Fatal(msg string) {
+func (l BaseCallback) Fatal(msg string) {
 	fmt.Println(Error.Render("[ FATAL ] " + msg))
 }
 
-func (l NormalCallback) Prompt(msg string, def bool) bool {
-	var token string
+func (l BaseCallback) Prompt(msg string, def bool) bool {
+	token := " [y/N] "
 	if def {
 		token = " [Y/n] "
-	} else {
-		token = " [y/N] "
 	}
 
 	fmt.Print(Info.Render(msg) + token)
 
 	scanner := bufio.NewScanner(os.Stdin)
-	usr := scanner.Text()
-	usr = strings.Trim(strings.ToLower(usr), " ")
+	scanner.Scan()
 
-	if usr == "y" || usr == "yes" {
+	usr := strings.TrimSpace(strings.ToLower(scanner.Text()))
+
+	switch usr {
+	case "y", "yes":
 		return true
-	}
-
-	if usr == "n" || usr == "no" {
+	case "n", "no":
 		return false
+	default:
+		return def
 	}
+}
 
-	return def
+type NormalCallback struct {
+	BaseCallback
+}
+
+type VerboseCallback struct {
+	BaseCallback
 }
 
 func (l VerboseCallback) Log(msg string) {
 	fmt.Println(Muted.Render("[  LOG  ] " + msg))
-}
-
-func (l VerboseCallback) Info(msg string) {
-	fmt.Println(Info.Render("[ INFO  ]") + " " + msg)
-}
-
-func (l VerboseCallback) Success(msg string) {
-	fmt.Println(Success.Render("[SUCCESS]") + " " + msg)
-}
-
-func (l VerboseCallback) Warn(msg string) {
-	fmt.Println(Warning.Render("[WARNING]") + " " + msg)
-}
-
-func (l VerboseCallback) Error(msg string) {
-	fmt.Println(Error.Render("[ ERROR ]") + " " + msg)
-}
-
-func (l VerboseCallback) Fatal(msg string) {
-	fmt.Println(Error.Render("[ FATAL ] " + msg))
-}
-
-func (l VerboseCallback) Prompt(msg string, def bool) bool {
-	var token string
-	if def {
-		token = " [Y/n] "
-	} else {
-		token = " [y/N] "
-	}
-
-	fmt.Print(Info.Render(msg) + token)
-
-	scanner := bufio.NewScanner(os.Stdin)
-	usr := scanner.Text()
-	usr = strings.Trim(strings.ToLower(usr), " ")
-
-	if usr == "y" || usr == "yes" {
-		return true
-	}
-
-	if usr == "n" || usr == "no" {
-		return false
-	}
-
-	return def
 }
